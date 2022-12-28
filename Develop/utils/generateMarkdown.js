3287d712af8d2964882fce199fbd8110f39f2bfa
N Z <nelectrik@gmail.com>
2022-12-29 01:11:28 +1030

    Fixing the code

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,16 +2,26 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license !== "None") {
-    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+    return `![MIT license](https://img.shields.io/badge/license-${license}-blue.svg)`;
   }
+  // else if (license !== "None") {
+  //   return `![APACHE 2.0 license](https://img.shields.io/badge/license-${APACHE}-blue.svg)`;
+  // } else if (license !== "None") {
+  //   return `![GPL 3.0 license](https://img.shields.io/badge/license-${GPL}-blue.svg)`;
+  // } else if (license !== "None") {
+  //   return `![BSD 3.0 license](https://img.shields.io/badge/license-${BSD}-blue.svg)`;
+  // }
+
   return "";
 }
+
+//['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license !== "None") {
-    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+    return ` [License #licenses])`;
   }
   return "";
 }
@@ -29,14 +39,55 @@ function renderLicenseSection(license) {
 function generateMarkdown(data) {
   return `# ${data.title}
 ${renderLicenseBadge(data.license)}  
+
+
+
+
+
+
+## Desscription
+${data.description}
+
+## Table of Contents
+  
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Test](#test)
+  * [Questions](#questions)
+  * [Github](#github)
+  * [License](#license)
+  
+  ## Installation
+  To install necesary dependencies, run the following command:
+  
+  \`\`\`
+  ${data.installationCmd}
+  \`\`\`
+  
+  ## Usage
+  > ${data.usage}
+  
+  ## Contributing
+  > ${data.ontributing}
+  
+  ## Test 
+  To test the application, please follow the below:
+  \`\`\`
+  ${data.test}
+  \`\`\`
+  ## Questions
+  > Should you have any questions, please send email to ${data.email}. 
+  
+  ## Github
+  > https://github.com/${data.username}
+  
+  
+  ## License 
+  > ${
+    data.license === "None" ? data.license : renderLicenseSection(data.license)
+  }
 `;
 }
 
 module.exports = generateMarkdown;
-
-// {
-//   type: 'list',
-//   name: 'license',
-//   message: 'What kind of license should your project have?',
-//   choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
-// },
